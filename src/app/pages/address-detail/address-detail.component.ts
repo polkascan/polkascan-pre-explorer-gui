8782968882f6c398d69dbc3bd947b419453888b3
Arjan Zijderveld <arjan@dynora.nl>
2019-05-14 15:52:06 +0200

    Added recent transactions on account page

--- src/app/pages/address-detail/address-detail.component.ts
+++ src/app/pages/address-detail/address-detail.component.ts
@@ -4,6 +4,8 @@ import {BalanceTransferService} from '../../services/balance-transfer.service';
 import {DocumentCollection} from 'ngx-jsonapi';
 import {BalanceTransfer} from '../../classes/balancetransfer.class';
 import {environment} from "../../../environments/environment";
+import {Extrinsic} from "../../classes/extrinsic.class";
+import {ExtrinsicService} from "../../services/extrinsic.service";
 
 
 @Component({
@@ -14,11 +16,17 @@ import {environment} from "../../../environments/environment";
 export class AddressDetailComponent implements OnInit {
 
   public balanceTransfers: DocumentCollection<BalanceTransfer>;
+  public extrinsics: DocumentCollection<Extrinsic>;
+
   public account: string;
   public networkTokenDecimals: number;
   public networkTokenSymbol: string;
 
-  constructor(private balanceTransferService: BalanceTransferService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private balanceTransferService: BalanceTransferService,
+    private extrinsicService: ExtrinsicService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
 
@@ -32,6 +40,16 @@ export class AddressDetailComponent implements OnInit {
         remotefilter: {address: this.account},
         page: { number: 0}
       }).subscribe(balanceTransfers => (this.balanceTransfers = balanceTransfers));
+
+      let params = {
+        page: {number: 0, size: 25},
+        remotefilter:{address: val.id},
+      };
+
+      this.extrinsicService.all(params).subscribe(extrinsics => {
+        this.extrinsics = extrinsics;
+      });
+
     });
   }
 
