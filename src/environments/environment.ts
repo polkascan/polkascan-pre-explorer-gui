d506817356530a4286b4ab5258abf806d385ffef
Arjan Zijderveld <arjan@dynora.nl>
2019-06-12 19:41:28 +0200

    Encode address fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   jsonApiRootUrl: 'http://127.0.0.1:8000/',
-  networkName: 'Alexander',
+  networkName: 'Dev',
   networkTokenSymbol: 'DOT',
   networkTokenDecimals: 15
 };
