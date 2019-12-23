f95e9d1c12f90e5ae27a5f7e73f7245914b18a51
Arjan Zijderveld <arjan@web3scan.eu>
2019-12-23 10:35:58 +0100

    Added missing @Autoregister()

--- src/app/services/treasury-proposal.service.ts
+++ src/app/services/treasury-proposal.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import {Service} from 'ngx-jsonapi';
+import {Autoregister, Service} from 'ngx-jsonapi';
 import {TreasuryProposal} from '../classes/treasury-proposal.class';
 
 @Injectable({
   providedIn: 'root'
 })
+@Autoregister()
 export class TreasuryProposalService extends Service<TreasuryProposal> {
   public resource = TreasuryProposal;
   public type = 'treasuryproposal';
