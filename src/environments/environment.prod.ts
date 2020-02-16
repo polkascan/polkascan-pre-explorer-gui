c5284f881c50284d913d413f0fa012fcc14b8e58
Arjan Zijderveld <arjan@web3scan.eu>
2020-02-16 23:23:32 +0100

    environment.prod.ts tweaks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  jsonApiRootUrl: 'https://polkascan.io/kusama/api/v1/',
+  jsonApiRootUrl: 'https://host-01.polkascan.io/kusama/api/v1/',
   networkName: 'Kusama',
   networkTokenSymbol: 'KSM',
   networkTokenDecimals: 12
