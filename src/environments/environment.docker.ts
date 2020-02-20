632d63a994a97017c15e67b12b8204d33e8bde28
Arjan Zijderveld <arjan@web3scan.eu>
2020-02-20 12:02:41 +0100

    PRE 0.5 release refactor

--- src/environments/environment.docker.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  jsonApiRootUrl: $ENV.API_URL,
-  networkName: $ENV.NETWORK_NAME,
-  networkTokenSymbol: $ENV.NETWORK_TOKEN_SYMBOL,
-  networkTokenDecimals: $ENV.NETWORK_TOKEN_DECIMALS
-};
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  jsonApiRootUrl: 'https://host-01.polkascan.io/kusama/api/v1/',
-  networkName: 'Kusama',
-  networkTokenSymbol: 'KSM',
-  networkTokenDecimals: 12
-};
